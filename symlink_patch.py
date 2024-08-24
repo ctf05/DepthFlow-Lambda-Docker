ea40e428e31d2891bbd6334f6e241b51a00c422e
Christian Beadling <christianbeadling@gmail.com>
2024-08-23 22:27:54 -0400

    change symlink

--- symlink_patch.py
+++ symlink_patch.py
@@ -1,12 +1,7 @@
+import click
+click.confirm = lambda *args, **kwargs: True
+
 from Broken import BrokenPath
 from unittest.mock import Mock
 
-def mock_symlink(virtual, real, **kwargs):
-    print(f"Mocked symlink: {virtual} -> {real}")
-    return virtual
-
-BrokenPath.symlink = mock_symlink
-
-# Mock click.confirm to always return True
-import click
-click.confirm = lambda *args, **kwargs: True
+BrokenPath.symlink = Mock()
