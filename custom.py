80e5643d4703b193594387939594ff8597d387a7
Christian Beadling <christianbeadling@gmail.com>
2024-08-25 17:08:21 -0400

    Update custom.py to change input type

--- custom.py
+++ custom.py
@@ -80,7 +80,7 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
 
 def lambda_handler(event, context):
     try:
-        body = event.get('body', {})
+        body = json.loads(event.get('body', '{}'))
 
         image_base64 = body.get('image')
         depth_base64 = body.get('depth')
